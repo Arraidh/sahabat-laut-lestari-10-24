8af85a0dc7e381f57c911d8bd4f9c08405985ad2
Kadek Ari Dharmika <ari.darmika@gmail.com>
2024-10-12 15:59:08 +0800

    feat: add navbar component and authentification handler

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,50 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Icon } from "@chakra-ui/react";
+import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { GiDoubleFish } from "react-icons/gi";
+import { FaSignOutAlt } from "react-icons/fa";
+import { BiLogIn } from "react-icons/bi";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter();
+  const [accessToken, setAccessToken] = useState(false);
+  useEffect(() => {
+    setAccessToken(localStorage?.getItem("token") || "");
+  }, []);
+
+  function handleLogout() {
+    localStorage?.removeItem("token");
+    setAccessToken(false);
+    router.reload();
+  }
   return (
     <>
-      <div className="w-full">
-        <Link href="/login" className="ml-auto">
-          <Button colorScheme="teal" variant={"solid"}>
-            Login
+      <div className="flex justify-between items-center w-full bg-white p-2 shadow-lg rounded-md">
+        <GiDoubleFish size={36} color="teal" />
+        {accessToken ? (
+          <Button
+            colorScheme={"red"}
+            variant={"solid"}
+            size={"sm"}
+            className="flex items-center gap-2"
+            onClick={handleLogout}
+          >
+            <FaSignOutAlt /> Logout
           </Button>
-        </Link>
+        ) : (
+          <Link href="/login" className="ml-auto">
+            <Button
+              colorScheme="teal"
+              variant={"solid"}
+              size={"sm"}
+              className="flex items-center gap-2"
+            >
+              <BiLogIn /> Login
+            </Button>
+          </Link>
+        )}
       </div>
     </>
   );
